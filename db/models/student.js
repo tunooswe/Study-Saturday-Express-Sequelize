f31f980ed026994cdfe3ef3a22fad606efd04eca
Tun Oo Swe <tunooswe@gmail.com>
2020-10-05 20:59:08 -0400

    student model created

--- db/models/student.js
+++ db/models/student.js
@@ -1,8 +1,33 @@
-'use strict';
+"use strict";
 
-const Sequelize = require('sequelize');
-const db = require('../db');
+const Sequelize = require("sequelize");
+const db = require("../db");
 
-const Student = db.define();
+const Student = db.define("student", {
+  firstName: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+  lastName: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+  email: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate: {
+      isEmail: true,
+    },
+  },
+});
+
+Student.beforeCreate((student) => {
+  const studentFirstName = student.firstName;
+  const studentLastName = student.lastName;
+  student.firstName =
+    studentFirstName[0].toUpperCase() + studentFirstName.slice(1);
+  student.lastName =
+    studentLastName[0].toUpperCase() + studentLastName.slice(1);
+});
 
 module.exports = Student;
