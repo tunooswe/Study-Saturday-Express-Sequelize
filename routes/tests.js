5433642a77e6b6f99dc750bad5afbbc446fb2008
Tun Oo Swe <tunooswe@gmail.com>
2020-10-05 22:01:28 -0400

    test routes completed

--- routes/tests.js
+++ routes/tests.js
@@ -1,4 +1,43 @@
-const router = require('express').Router();
+const router = require("express").Router();
+const Test = require("../db/models/test");
+const Student = require("../db/models/student");
 
+router.get("/", async (req, res, next) => {
+  try {
+    const tests = await Test.findAll();
+    res.send(tests);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.get("/:id", async (req, res, next) => {
+  try {
+    const test = await Test.findById(req.params.id);
+    res.send(test);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.post("/student/:studentId", async (req, res, next) => {
+  try {
+    const student = await Student.findById(req.params.studentId);
+    const test = await Test.create(req.body);
+    const studentTest = await test.setStudent(student);
+    res.status(201).send(studentTest);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.delete("/:id", async (req, res, next) => {
+  try {
+    await Test.destroy({ where: { id: req.params.id } });
+    res.status(204).send();
+  } catch (error) {
+    next(error);
+  }
+});
 
 module.exports = router;
